74a62fff69b450ac6b3260d60f0d3763b08f85b5
James Montgomery <James.Brian.Montgomery@gmail.com>
2023-01-08 21:06:13 -0500

    Add code for text to speech and playing audio.

--- src/trickybot-pycord.py
+++ src/trickybot-pycord.py
@@ -4,17 +4,77 @@ import os,discord,threading,asyncio
 
 from discord.ext import commands
 
+from discord import Message as DiscordMessage
+import logging
+from base import Message, Conversation
+from constants import (
+    BOT_INVITE_URL,
+    DISCORD_BOT_TOKEN,
+    EXAMPLE_CONVOS,
+    ACTIVATE_THREAD_PREFX,
+    MAX_THREAD_MESSAGES,
+    SECONDS_DELAY_RECEIVING_MSG,
+)
+import asyncio
+from utils import (
+    logger,
+    should_block,
+    close_thread,
+    is_last_message_stale,
+    discord_message_to_message,
+)
+import completion
+from completion import generate_completion_response, process_response, process_text_response
+from moderation import (
+    moderate_message,
+    send_moderation_blocked_message,
+    send_moderation_flagged_message,
+)
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", level=logging.INFO
+)
+
+import pyttsx3
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init(driverName='sapi5')
+
+def initialize_text_to_speech():
+    text = "Hello, my name is trickybot. I am an artificial intelligence designed to interact with users in this channel"
+
+    print(f"Voices are {engine.getProperty('voices')}")
+
+    # Set the volume and rate of speech
+    engine.setProperty('volume', 1.0)
+    engine.setProperty('rate', 150)
+
+    #Use Cortana (Microsoft EVA)
+    engine.setProperty('voice',"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enUS_EvaM")
+
+def text_to_speech(text:str)->str:
+
+    output_file = "speech.mp3"
+
+    #Save the audio to a file
+    engine.save_to_file(text, output_file)
+    #engine.say(text)
+    engine.runAndWait()
+
+    return output_file
+
 #import whisper
 
 #model = whisper.load_model("base")
 
 #Use Whisper to convert recorded audio files to text
-def convert_audio_to_text()->str:
-    result = model.transcribe("audio.mp3")
+def convert_audio_to_text(filename:str)->str:
+    result = model.transcribe(filename)
     #print(result["text"])
     return result[text]
 
-
+#Retain a dictionary of messages for all users
+messages = {}
 connections = {}
 
 intents = discord.Intents.all()
@@ -38,14 +98,56 @@ class Sinks(Enum):
     m4a = discord.sinks.M4ASink()
 
 
+def add_to_messages(user, text:str):
+    #If the user isn't in the dictionary, initialize a list of messages for them
+    if not user.id in messages:
+        messages[user] = []
+    messages[user].insert(0, Message(user = user.name, text=text))
+
+async def send_to_model(user):    
+    try:
+        # generate the response
+        #async with thread.typing():
+        response_data = await generate_completion_response(
+        messages=messages[user], user=user
+        )    
+
+        # send response
+        text_response,status_response=await process_text_response(
+            user=user, response_data=response_data
+        )
+        return text_response,status_response
+    except Exception as e:
+        logger.exception(e)
+
 async def finished_callback(sink, channel: discord.TextChannel, *args):
     recorded_users = [f"<@{user_id}>" for user_id, audio in sink.audio_data.items()]
-    await sink.vc.disconnect()
+    #await sink.vc.disconnect()
 
     for user_id, audio in sink.audio_data.items():
         #print(f"Audio file location was {audio.file} {user_id}.{sink.encoding}")
+
+        #Write to a file
         with open(f"{user_id}.{sink.encoding}", "wb") as f:
             f.write(audio.file.getbuffer())
+
+        #Convert to text using whisper
+        converted_text = "Trickybot, what's the capital of Idaho?"#convert_audio_to_text(f"{user_id}.{sink.encoding}")
+
+        #Get the user from the provided ID        
+        user = bot.get_user(user_id)
+        print(f"User was {user.name} and text was {converted_text}")
+
+        #Add the user prompt to the conversation history
+        add_to_messages(user, converted_text)
+
+        #Send the converted text to the model
+        ai_response = await send_to_model(user)
+
+        print(f"AI response was {ai_response}")
+
+        #Convert the AI response to an audio file
+        text_to_speech(ai_response)
 
     files = [
         discord.File(audio.file, f"{user_id}.{sink.encoding}")
@@ -61,6 +163,17 @@ async def finished_callback(sink, channel: discord.TextChannel, *args):
 #    name = name or ctx.author.name
 #    await ctx.respond(f"Hello {name}!")
 
+#@commands.command()
+async def play(ctx: commands.Context, query: str):
+    """Plays a file from the local filesystem"""
+
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+    ctx.voice_client.play(
+        source, after=lambda e: print(f"Player error: {e}") if e else None
+    )
+
+    await ctx.send(f"Now playing: {query}")
+
 @bot.command()
 async def add(ctx: commands.Context, left: int, right: int):
     """Adds two numbers together."""
@@ -70,6 +183,10 @@ async def add(ctx: commands.Context, left: int, right: int):
 async def hello(ctx, name: str = None):
     name = name or ctx.author.name
     await ctx.send(f"Hello {name} {ctx.author.id}!")
+
+async def timer(ctx):
+    await asyncio.sleep(4)
+    await stop(ctx)
 
 @bot.command()
 async def record(ctx):
@@ -81,6 +198,8 @@ async def record(ctx):
     vc = await voice.channel.connect()
     connections.update({ctx.guild.id: vc})
 
+    await play(ctx, "speech.mp3")
+
     print("Starting record command")
     vc.start_recording(
         discord.sinks.WaveSink(),
@@ -88,12 +207,13 @@ async def record(ctx):
         ctx.channel,
     )
 
-    #timer = threading.Timer(5.0, stop, args=[ctx])
-    #timer.start()
-    await asyncio.sleep(5)
-    await stop(ctx)
+    #await asyncio.sleep(5)
+    #await stop(ctx)
     
+    task = asyncio.create_task(timer(ctx))
+
     await ctx.send("The recording has started!")
+    await task
 
 @bot.slash_command()
 async def vc(ctx):
@@ -110,4 +230,9 @@ async def stop(ctx: discord.ApplicationContext):
     else:
         await ctx.send("Not recording in this guild.")
 
-bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+@bot.event
+async def on_ready():
+    initialize_text_to_speech()
+    print("I am online")
+
+bot.run(DISCORD_BOT_TOKEN)#os.environ.get('DISCORD_BOT_TOKEN'))
